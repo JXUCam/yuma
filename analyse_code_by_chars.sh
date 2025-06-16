7c7f7236dc90ae9727205e622b08f517341be70c
Jiaying Xu <jiaying.xu@worldpay.com>
2025-06-16 08:14:28 +0100

    to print out the overall profile of the code duplication in one go

--- analyse_code_by_chars.sh
+++ analyse_code_by_chars.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ##分析编码，统计重码程度，评估离散表现。
-head -8105 code.txt | awk '{print $2, $1}' | sort -d | cut -d' ' -f1 | uniq -c | sort -n
+head -8105 code.txt | awk '{print $2, $1}' | sort -d | awk '{print $1}' | uniq -c | sort -n | awk '{print $1}' | uniq -c
 
